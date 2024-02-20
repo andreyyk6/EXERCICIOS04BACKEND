7bbafdbd76dce021f88ee33652485a952c9d8ae2
andreyyk6 <martins.andrey@escola.pr.gov.br>
2024-02-20 10:50:29 -0300

    exercicios 04

---
+++ exercicio.js
@@ -0,0 +1,9 @@
+// Solicita ao usuário para digitar um número
+var numero = prompt("Digite um número:");
+
+// Verifica se o resto da divisão por 2 é igual a 0
+if (numero % 2 === 0) {
+    console.log("O número é par.");
+} else {
+    console.log("O número é ímpar.");
+}
